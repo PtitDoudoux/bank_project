24e5a3d9ba189c9b55b554f8c9e21a068eea25c1
Thomas DUDOUX <thomas.dudoux@gmail.com>
2017-07-04 11:13:45 +0200

    Directory and files structure done

--- app/customers.c
+++ app/customers.c
@@ -1,37 +1,43 @@
-#include <base.h>
-#include <csv.h>
-#include <bcrypt.h>
+#include <memory.h>
+#include "../core/include/base.h"
+#include "../core/libs/libcsv/csv.h"
+#include "../core/libs/bcrypt/crypt_blowfish/crypt_blowfish.h"
+
 
 /**
  * Structure who represent a client in the CSV DB
  */
 struct Customers {
   int id;
-  char* lastname[20];
-  char* firstname[20];
-  char* job[50];
-  char* tel_num[10];
-  char* password[60];
+  char lastname[20];
+  char firstname[20];
+  char job[50];
+  char tel_num[10];
+  char password[60];
 };
 
 /**
  * Function for adding a client in the CSV DB
- * @param Customers client : The client to be added
- * @return void : Return a message about the status of the action executed
+ * @param firstname The firstname of the customer
+ * @param lastname The lastname of the customer
+ * @param job The job of the customer
+ * @param tel_num The telephone number of the customer
+ * @param password The password of the customer
+ * @return void Return a message about the status of the action executed
  */
-void add_customer(char* firstname[20], char* lastname[20], char* job[50], char* tel_num[10], char* password[60]) {
+void add_customer(char firstname[20], char lastname[20], char job[50], char tel_num[10], char password[60]) {
     struct Customers client;
-    client.firstname = firstname;
-    client.lastname = lastname;
-    client.job = job;
-    client.tel_num = tel_num;
+    strcpy(client.firstname, firstname);
+    strcpy(client.lastname, lastname);
+    strcpy(client.job, job);
+    strcpy(client.tel_num, tel_num);
     // client.password = ;
 }
 
 /**
  * Function for deleting a client in the CSV DB
- * @param int id : The id of the client to be deleted
- * @return void : Return a message about the status of the action executed
+ * @param id The id of the client to be deleted
+ * @return void Return a message about the status of the action executed
  */
 void delete_customer(int id) {
 
@@ -39,8 +45,8 @@ void delete_customer(int id) {
 
 /**
  * Function for deleting a client in the CSV DB
- * @param int id : The id of the client to be modified
- * @return void : Return a message about the status of the action executed
+ * @param id The id of the client to be modified
+ * @return void Return a message about the status of the action executed
  */
 void modify_client(int id) {
 
---
+++ cmake-build-debug/CMakeFiles/bank_project.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/dudoux/Documents/Cours/langage_c/bank_project/app/accounts.c" "/home/dudoux/Documents/Cours/langage_c/bank_project/cmake-build-debug/CMakeFiles/bank_project.dir/app/accounts.c.o"
+  "/home/dudoux/Documents/Cours/langage_c/bank_project/app/admnistration.c" "/home/dudoux/Documents/Cours/langage_c/bank_project/cmake-build-debug/CMakeFiles/bank_project.dir/app/admnistration.c.o"
+  "/home/dudoux/Documents/Cours/langage_c/bank_project/app/customers.c" "/home/dudoux/Documents/Cours/langage_c/bank_project/cmake-build-debug/CMakeFiles/bank_project.dir/app/customers.c.o"
+  "/home/dudoux/Documents/Cours/langage_c/bank_project/app/history.c" "/home/dudoux/Documents/Cours/langage_c/bank_project/cmake-build-debug/CMakeFiles/bank_project.dir/app/history.c.o"
+  "/home/dudoux/Documents/Cours/langage_c/bank_project/app/operations.c" "/home/dudoux/Documents/Cours/langage_c/bank_project/cmake-build-debug/CMakeFiles/bank_project.dir/app/operations.c.o"
+  "/home/dudoux/Documents/Cours/langage_c/bank_project/core/file.c" "/home/dudoux/Documents/Cours/langage_c/bank_project/cmake-build-debug/CMakeFiles/bank_project.dir/core/file.c.o"
+  "/home/dudoux/Documents/Cours/langage_c/bank_project/core/libs/bcrypt/bcrypt.c" "/home/dudoux/Documents/Cours/langage_c/bank_project/cmake-build-debug/CMakeFiles/bank_project.dir/core/libs/bcrypt/bcrypt.c.o"
+  "/home/dudoux/Documents/Cours/langage_c/bank_project/core/libs/bcrypt/crypt_blowfish/crypt_blowfish.c" "/home/dudoux/Documents/Cours/langage_c/bank_project/cmake-build-debug/CMakeFiles/bank_project.dir/core/libs/bcrypt/crypt_blowfish/crypt_blowfish.c.o"
+  "/home/dudoux/Documents/Cours/langage_c/bank_project/core/libs/bcrypt/crypt_blowfish/crypt_gensalt.c" "/home/dudoux/Documents/Cours/langage_c/bank_project/cmake-build-debug/CMakeFiles/bank_project.dir/core/libs/bcrypt/crypt_blowfish/crypt_gensalt.c.o"
+  "/home/dudoux/Documents/Cours/langage_c/bank_project/core/libs/bcrypt/crypt_blowfish/wrapper.c" "/home/dudoux/Documents/Cours/langage_c/bank_project/cmake-build-debug/CMakeFiles/bank_project.dir/core/libs/bcrypt/crypt_blowfish/wrapper.c.o"
+  "/home/dudoux/Documents/Cours/langage_c/bank_project/core/libs/libcsv/libcsv.c" "/home/dudoux/Documents/Cours/langage_c/bank_project/cmake-build-debug/CMakeFiles/bank_project.dir/core/libs/libcsv/libcsv.c.o"
+  "/home/dudoux/Documents/Cours/langage_c/bank_project/core/utils.c" "/home/dudoux/Documents/Cours/langage_c/bank_project/cmake-build-debug/CMakeFiles/bank_project.dir/core/utils.c.o"
+  "/home/dudoux/Documents/Cours/langage_c/bank_project/main.c" "/home/dudoux/Documents/Cours/langage_c/bank_project/cmake-build-debug/CMakeFiles/bank_project.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
